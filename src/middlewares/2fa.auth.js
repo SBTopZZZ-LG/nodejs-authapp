9c2a95d94e95f7ab4a17bfb5044a50c62dd78a78
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2022-03-01 14:56:38 +0530

    Now resetting password with 2fa is possible

---
+++ src/middlewares/2fa.auth.js
@@ -0,0 +1,69 @@
+const User = require("../models/user")
+
+module.exports = async (req, res, next) => {
+    try {
+        const query = req.query
+        const body = req.body
+
+        const id = query["id"]
+        if (!id)
+            return res.status(400).send({
+                error: "missingId",
+                result: {}
+            })
+
+        const first_question_answer = body["first_question_answer"]
+        const second_question_answer = body["second_question_answer"]
+        const third_question_answer = body["third_question_answer"]
+        if (!first_question_answer)
+            return res.status(400).send({
+                error: "firstQuestionAnswerMissing",
+                result: {}
+            })
+        else if (!second_question_answer)
+            return res.status(400).send({
+                error: "secondQuestionAnswerMissing",
+                result: {}
+            })
+        else if (!third_question_answer)
+            return res.status(400).send({
+                error: "thirdQuestionAnswerMissing",
+                result: {}
+            })
+
+        const user = await User.findById(id).exec()
+        if (!user)
+            return res.status(404).send({
+                error: "userNotFound",
+                result: {}
+            })
+
+        if (!user["two_factor_auth"]["isEnabled"])
+            return res.status(403).send({
+                error: "2faDisabled",
+                result: {}
+            })
+
+        if (!user["two_factor_auth"]["first_question"]["question"] ||
+            !user["two_factor_auth"]["second_question"]["question"] || !user["two_factor_auth"]["third_question"]["question"])
+            return res.status(403).send({
+                error: "missingQuestions",
+                result: {}
+            })
+
+        if (user["two_factor_auth"]["first_question"]["answer"] !== first_question_answer ||
+            user["two_factor_auth"]["second_question"]["answer"] !== second_question_answer || user["two_factor_auth"]["third_question"]["answer"] !== third_question_answer)
+            return res.status(403).send({
+                error: "incorrectAnswer",
+                result: {}
+            })
+
+        req.user = user
+        return next()
+    } catch (e) {
+        return res.status(500).send({
+            error: e.toString(),
+            result: {}
+        })
+    }
+}
---
+++ src/routes/user/2fa/reset.password.js
@@ -0,0 +1,50 @@
+const Router = require("express").Router()
+const TwoFAAuth = require("../../../middlewares/2fa.auth")
+const User = require("../../../models/user")
+const Verification = require("../../../models/verify")
+
+Router.post("/user/2fa/resetPassword", TwoFAAuth, async (req, res, next) => {
+    try {
+        const body = req.body
+
+        const new_password = body["password"]
+        if (!new_password)
+            return res.status(400).send({
+                error: "missingPassword",
+                result: {}
+            })
+
+        if (typeof new_password !== "string")
+            return res.status(400).send({
+                error: "passwordIsNotAValidString",
+                result: {}
+            })
+
+        if (new_password.length < 6)
+            return res.status(400).send({
+                error: "passwordIsTooShort",
+                result: {}
+            })
+        else if (new_password.length > 16)
+            return res.status(400).send({
+                error: "passwordIsTooLong",
+                result: {}
+            })
+
+        const email = req.user["email"]["email"]
+
+        await Verification.createResetPasswordVerification(email, new_password)
+
+        return res.status(200).send({
+            error: null,
+            result: null
+        })
+    } catch (e) {
+        return res.status(500).send({
+            error: e.toString(),
+            result: null
+        })
+    }
+})
+
+module.exports = Router
